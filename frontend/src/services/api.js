8fe617c68d7f979d275dd08725c7eda7ee213a6e
phoniccgirish <gy63862829@gmail.com>
2026-07-23 00:25:57 +0530

    frontend in CORS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 const TOKEN_KEY = "cpf_token";
 
-import axios from "axios";
-
-const TOKEN_KEY = "cpf_token";
-
 export const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   headers: {
